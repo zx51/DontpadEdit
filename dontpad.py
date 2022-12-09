0dbadf32120411fbfa0ad6202c209c9aae7e5dfa
Iago Braga da Silva <34430008+zx51@users.noreply.github.com>
2022-12-09 10:25:54 -0300

    Update dontpad.py

--- dontpad.py
+++ dontpad.py
@@ -5,19 +5,39 @@ import argparse
 
 # Trata argumentos
 parser = argparse.ArgumentParser(description="Modifica paginas dontpad")
-parser.add_argument('-s', '--string', required=True, help="Insere a string na página.")
-parser.add_argument('-p', '--page', required=True, help="Estipula a página a ser alterada")
+parser.add_argument('-s', '--string', type=str, required=False, help="Insere a string na página.", default="")
+parser.add_argument('-p', '--page', type=str, required=True, help="Estipula a página a ser alterada")
+parser.add_argument('-a', '--action', type=str, required=True, help="Escolhe entre os modos EDIT/VIEW", default="edit")
 args = parser.parse_args()
 
 # Armazena argumentos
-page = str(args.page)
-text = str(args.string)
+page = args.page
+text = args.string
+action = args.action
 
-# Prepara requisição
-url = "https://api.dontpad.com/" + page
-data = {
-	'text': text
-}
+def dontpadEdit():
+	url = "https://api.dontpad.com/"   page
+	payload = {
+		'text': text
+	}
+	requisicao = requests.post(url, data=payload)
+	print(f"A página {page} foi modificada para o texto: {text}")
+	exit(0)
 
-requisicao = requests.post(url, data=data)
-print(f"A página {page} foi modificada para o texto: {text}")
+def dontpadView():
+	url = "https://api.dontpad.com/"   page   ".body.json"
+	params = {
+		'lastModified': 0
+	}
+	requisicao = requests.get(url, params=params)
+	text = requisicao.json()
+	print(text["body"])
+	exit(0)
+
+
+if action == "edit":
+	dontpadEdit()
+elif action == "view":
+	dontpadView()
+else:
+	print("Algo deu errado! digite: \"edit\" ou \"view\"")
